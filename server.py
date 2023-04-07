28259791b3de8899b9c2b3ce3546deb654db3828
Yi <119040122+YiZaha0@users.noreply.github.com>
2023-04-07 16:45:51 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,11 +1,14 @@
 from aiohttp import web
 
+routes = web.RouteTableDef()
+
+@routes.get("/", allow_head=True)
 async def root_handler(request):
     return web.json_response("Online")
 
 async def run_web_server():
     app = web.Application()
-    app.add_routes([web.get("/", root_handler)])
+    app.add_routes(routes)
     runner = web.AppRunner(app)
     await runner.setup()
     await web.TCPSite(runner, "0.0.0.0", 3000).start()
